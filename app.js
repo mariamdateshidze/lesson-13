50245f93cab323ba578c9ed6aa65f7636069cdc6
Mariam <74742426+mariamdateshidze@users.noreply.github.com>
2022-04-04 17:04:26 +0400

    add html and js files

---
+++ app.js
@@ -0,0 +1,23 @@
+let userName = 'Mariam';
+let userAge = 19;
+let isStudent = true;
+let userSurname;
+let userJob= null;
+let userNan = NaN;
+
+
+// console.log(typeof userName, typeof userAge, typeof isStudent, typeof userSurname, typeof userJob, typeof userNan);
+
+let myName="Mariam";
+let myHobby="painting";
+let yearOfBirth = 2002;
+let currentYear = 2022;
+
+let myText = `My name is ${myName} I am ${currentYear-yearOfBirth} old and my hobby is ${myHobby}`;
+
+// console.log(myText);
+
+
+// console.log("My name is" + " " + myName + "i am" + " " + (currentYear-yearOfBirth) + " " + "years old and my hobby is" +" "+ myHobby);
+
+
